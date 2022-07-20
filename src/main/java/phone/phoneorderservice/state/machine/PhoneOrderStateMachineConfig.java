da0a9bca938aaa67dc9776352823c8c95619f4ef
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-20 17:42:13 +0300

    Refactoring Order States and Events. CLoses #10.

--- src/main/java/phone/phoneorderservice/state/machine/PhoneOrderStateMachineConfig.java
+++ src/main/java/phone/phoneorderservice/state/machine/PhoneOrderStateMachineConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import phone.phoneorderservice.domain.PhoneOrderEventEnum;
 import phone.phoneorderservice.domain.PhoneOrderStatusEnum;
 
@@ -24,5 +25,18 @@ public class PhoneOrderStateMachineConfig extends StateMachineConfigurerAdapter<
                 .end(PhoneOrderStatusEnum.DELIVERY_EXCEPTION)
                 .end(PhoneOrderStatusEnum.VALIDATION_EXCEPTION)
                 .end(PhoneOrderStatusEnum.ALLOCATION_EXCEPTION);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<PhoneOrderStatusEnum, PhoneOrderEventEnum> transitions) throws Exception {
+        transitions.withExternal()
+                    .source(PhoneOrderStatusEnum.NEW).target(PhoneOrderStatusEnum.VALIDATION_PENDING)
+                    .event(PhoneOrderEventEnum.VALIDATE_ORDER)
+                .and().withExternal()
+                    .source(PhoneOrderStatusEnum.NEW).target(PhoneOrderStatusEnum.VALIDATED)
+                    .event(PhoneOrderEventEnum.VALIDATION_PASSED)
+                .and().withExternal()
+                    .source(PhoneOrderStatusEnum.NEW).target(PhoneOrderStatusEnum.VALIDATED)
+                    .event(PhoneOrderEventEnum.VALIDATION_FAILED);
     }
 }
